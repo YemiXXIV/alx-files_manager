7a431fe7fafe63eafea24ebc1bf39e281e53ff0b
Mustapha <mustapha.yinusa@yahoo.com>
2024-10-22 15:00:27 +0100

    lets see

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-// const authenticate = require('../middlewares/authenticate');
+const authenticate = require('../middlewares/authenticate');
 const FilesController = require('../controllers/FilesController');
 
 router.get('/status', AppController.getStatus);
